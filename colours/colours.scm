de19883b33ac15e20f5b07025d2378a22cfdff35
absrnd <stuart@ifoundry.co.uk>
2015-06-30 16:58:22 +0000

    first sync

---
+++ colours/colours.scm
@@ -0,0 +1,38 @@
+;; colours.scm - playing with ANSI color codes
+;;
+
+(define (set-bgd! color)
+  "set the background colour (0-7) using ANSI codes"
+  (begin
+    (display "\x1b[")
+    (display (number->string (+ 40 color)))
+    (display "m")))
+
+(define (reset-ANSI)
+  "reset the ANSI terminal codes to default values"
+  (display "\x1b[0m"))
+
+
+(define (square color)
+  "display an ANSI (0-7) coloured space character."
+  (begin
+    (set-bgd! color)
+    (display " ")))
+
+(define (row row-list)
+  "display a row of coloured squares from list of 0-7 values"
+  (begin
+    (for-each square row-list)
+    (reset-ANSI)
+    (display "\n")))
+
+(define (rand-row length)
+  "create a row-list of length >0 containing random colours"
+  (if (< length 1)
+      '()
+      (cons (random-integer 8)
+	    (random-row (- length 1)))))
+
+(define (display-pattern size)
+  "output a random colour checker pattern of given size"
+  (letrec ((

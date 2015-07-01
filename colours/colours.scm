736b77f408937f92d2dc4e7234c386d6da20febb
Stuart Malcolm <stuart@ifoundry.co.uk>
2015-07-01 22:16:32 +0100

    removed doc-strings and changed to comments

--- colours/colours.scm
+++ colours/colours.scm
@@ -1,40 +1,39 @@
 ;; colours.scm - playing with ANSI color codes
 ;;
 
+;; set the background colour (0-7) using ANSI codes
 (define (set-bgd! color)
-  "set the background colour (0-7) using ANSI codes"
   (begin
     (display "\x1b[")
     (display (number->string (+ 40 color)))
     (display "m")))
 
+;; reset the ANSI terminal codes to default values
 (define (reset-ANSI)
-  "reset the ANSI terminal codes to default values"
   (display "\x1b[0m"))
 
-
+;; display an ANSI (0-7) coloured space character.
 (define (square color)
-  "display an ANSI (0-7) coloured space character."
   (begin
     (set-bgd! color)
     (display " ")))
 
+;; display a row of coloured squares from list of 0-7 values
 (define (row row-list)
-  "display a row of coloured squares from list of 0-7 values"
   (begin
     (for-each square row-list)
     (reset-ANSI)
     (display "\n")))
 
+;; create a row-list of length >0 containing random colours
 (define (rand-row length)
-  "create a row-list of length >0 containing random colours"
   (if (< length 1)
       '()
       (cons (random-integer 8)
 	    (rand-row (- length 1)))))
 
+;; output a random colour checker pattern of given size
 (define (display-pattern size)
-  "output a random colour checker pattern of given size"
   (let loop ((i 0))
     (row (rand-row size))
     (if (< i size)
